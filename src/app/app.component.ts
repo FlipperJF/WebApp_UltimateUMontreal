9f4d40563488fdcedd2aeb20b662b05bfe806d72
FlipperJF <jfmorincamo@gmail.com>
2017-02-08 12:50:44 -0500

    CSS fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { BannerComponent } from './components/banner/banner.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] ,
 })
 export class AppComponent {
   title = 'Hello world-----LOL!';
