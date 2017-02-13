27a5bb98784e78c46ef16731c36d37c05a5b9fd1
FlipperJF <jfmorincamo@gmail.com>
2017-02-12 22:23:12 -0500

    Laptop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material'
+import 'hammerjs'
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { ClubComponent } from './components/club/club.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    BannerComponent
+    BannerComponent,
+    ClubComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
